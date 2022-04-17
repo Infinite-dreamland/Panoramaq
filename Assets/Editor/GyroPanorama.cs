b32761e6d2bb3ebae992e58b668bd3d8e664245e
sunonsun123 <55424102+sunonsun123@users.noreply.github.com>
2022-04-18 03:13:28 +0800

    assets

---
+++ Assets/Editor/GyroPanorama.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GyroPanorama : MonoBehaviour
+{
+    private Quaternion gyroscopeQua;  
+    private Quaternion quatMult = new Quaternion(0, 0, 1, 0);  
+    private float speedH = 0.2f;   
+    protected void Start()
+    {
+        Input.gyro.enabled = true;
+        transform.eulerAngles = new Vector3(90, 90, 0);
+    }
+
+    protected void Update()
+    {
+        GyroModifyCamera();
+    }
+
+    protected void GyroModifyCamera()
+    {
+        gyroscopeQua = Input.gyro.attitude * quatMult;  
+        Camera.main.transform.localRotation = Quaternion.Slerp(
+            Camera.main.transform.localRotation,
+            gyroscopeQua,
+            speedH
+            );
+    }
+
+    protected void OnGUI()
+    {
+        GUI.skin.label.fontSize = Screen.width / 40;
+        GUILayout.Label("Orientation: " + Screen.orientation);
+        GUILayout.Label("input.gyro.eulerAngles: " + Input.gyro.attitude.eulerAngles);
+        GUILayout.Label("now eulerAngles: " + Camera.main.transform.eulerAngles);
+        GUILayout.Label("iphone width/font: " + Screen.width + " : " + GUI.skin.label.fontSize);
+    }
+}
